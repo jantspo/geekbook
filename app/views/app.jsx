ce08e2b4bb68188ca5236af24086cfa6d249ca16
John Sposato <jantspo@gmail.com>
2016-01-19 18:05:44 -0800

    initial commit

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import {Router, Route, browserHistory} from 'react-router'
+import Login from './login'
 import '~/app/assets/styles/app'
 
 class App extends React.Component {
   render() {
-    const { children } = this.props
+    const { children } = this.props;
 
     return (
       <div className='container'>
@@ -15,4 +17,10 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById('react'))
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={ App }>
+        <Route path="login" component={ Login }></Route>
+    </Route>
+  </Router>
+), document.getElementById('react'));
